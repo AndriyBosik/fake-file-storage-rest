2b68631a94ae0adb1a94795aae99d182f42b3b54
AndriyBosik <andriybosik2002@ukr.net>
2021-10-04 16:16:38 +0600

    Initial commit

---
+++ src/test/java/com/example/file/storage/FileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
